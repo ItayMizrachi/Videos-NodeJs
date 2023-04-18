58ecac5de6a6e07a5a9a2a6ef07cb8a2528eb580
ItayMizrachi <itaymiz98@gmail.com>
2023-04-18 09:20:55 +0300

    L 40

--- routes/videos.js
+++ routes/videos.js
@@ -40,6 +40,20 @@ router.get("/single/:id", async (req, res) => {
     }
 })
 
+router.get("/count", async (req, res) => {
+    try {
+        let perPage = req.query.perPage || 5;
+        // יקבל רק את כמות הרשומות בקולקשן
+        const count = await VideoModel.countDocuments({})
+        res.json({ count, pages: Math.ceil(count / perPage) })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+//http://localhost:3001/videos/count
+
 router.post("/", auth, async (req, res) => {
     let validBody = validateVideo(req.body);
     if (validBody.error) {
